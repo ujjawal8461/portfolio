a8c717b138100744641ae255ab923f5f683f2cb0
ujjawal8461 <ujjawal8461@gmail.com>
2026-03-06 00:18:35 +0530

    fixed order

--- app/page.tsx
+++ app/page.tsx
@@ -10,8 +10,8 @@ export default function Home() {
       <HeroSection />
       <TimeLineSection />
       <SkillSection />
-      <AboutSection />
       <ProjectsSection2 />
+      <AboutSection />
     </main>
   );
 }
